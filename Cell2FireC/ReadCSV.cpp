545174b6708473be6ee177a1e2889607e298afd1
fdobad <29801096+fdobad@users.noreply.github.com>
2023-05-17 22:28:04 -0400

    win compilado

--- Cell2FireC/ReadCSV.cpp
+++ Cell2FireC/ReadCSV.cpp
@@ -25,26 +25,26 @@ CSVReader::CSVReader(std::string filename, std::string delm){
 * Parses through csv file line by line and returns the data
 * in vector of vector of strings.
 */
-std::vector<std::vector<std::string>> CSVReader::getData(){
+std::vector<std::vector<std::string>> CSVReader::getData() {
 	std::ifstream file(this->fileName);
 	std::vector<std::vector<std::string> > dataList;
 	std::string line = "";
-// Iterate through each line and split the content using delimeter
-	if(this->fileName.substr(this->fileName.find_last_of(".") + 1) == "asc"){
-		int header=0;
-		while (getline(file, line)){
-			if (header<5){
+	// Iterate through each line and split the content using delimeter
+	if (this->fileName.substr(this->fileName.find_last_of(".") + 1) == "asc") {
+		int header = 0;
+		while (getline(file, line)) {
+			if (header < 5) {
 				std::vector<std::string> vec;
-				int start=0;
-				int end=0;
-				while((start=line.find_first_not_of(this->delimeter,end))!=std::string::npos){
-					end= line.find(this->delimeter,start);
-					vec.push_back(line.substr(start,end-start));
+				int start = 0;
+				int end = 0;
+				while ((start = line.find_first_not_of(this->delimeter, end)) != std::string::npos) {
+					end = line.find(this->delimeter, start);
+					vec.push_back(line.substr(start, end - start));
 				}
 				dataList.push_back(vec);
 				header++;
 			}
-			else{
+			else {
 				std::vector<std::string> vec;
 				boost::algorithm::split(vec, line, boost::is_any_of(this->delimeter));
 				dataList.push_back(vec);
@@ -52,16 +52,19 @@ std::vector<std::vector<std::string>> CSVReader::getData(){
 
 		}
 	}
-		std::vector<std::string> vec;
-		boost::algorithm::split(vec, line, boost::is_any_of(this->delimeter));
-		dataList.push_back(vec);
+
+	else {
+		while (getline(file, line)) {
+			std::vector<std::string> vec;
+			boost::algorithm::split(vec, line, boost::is_any_of(this->delimeter));
+			dataList.push_back(vec);
 		}
 	}
-	
+
 
 	// Close the File
 	file.close();
- 
+
 	return dataList;
 }
 
